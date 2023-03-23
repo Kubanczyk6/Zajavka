ce68ce08e1bdbad5268c4ee37dc8c250d4a42d4a
Kubanczyk6 <jakub.gradzki96@gmail.com>
2023-03-23 17:28:12 +0100

    commit 3 main

--- Person.java
+++ Person.java
@@ -1,9 +1,5 @@
 public class Person {
 
-int a =0;
-
-String ab="abc";
-
 
 int age = 20;
 
@@ -11,8 +7,7 @@ int age = 20;
 String surname = "kowalski";
 
 String name = "Kuba";
-String ar = " awa";
-String aw="aw";
+
 
 
 }
